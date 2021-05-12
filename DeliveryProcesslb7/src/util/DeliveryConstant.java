a2443f4618df30bc0b6c08056d72e0f47e055df0
TharukshiPerera <61499240+TharukshiPerera@users.noreply.github.com>
2021-05-12 15:27:49 +0530

    Add files via upload

---
+++ DeliveryProcesslb7/src/util/DeliveryConstant.java
@@ -0,0 +1,7 @@
+package util;
+
+public class DeliveryConstant {
+
+	public static final String DBLocation = "jdbc:mysql://127.0.0.1:3306/delivery-db";
+	public static final String DBDriver = "com.mysql.jdbc.Driver";
+}
